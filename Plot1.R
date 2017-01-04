21579bb0d67b13e2de73069fb45c22be7a036de6
hxpwork <hxpwork@126.com>
2017-01-04 08:07:12 +0800

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -12,7 +12,7 @@ datas <- read.table("household_power_consumption.txt",
         filter( Date == "1/2/2007" | Date =="2/2/2007") %>%
         mutate( Date=dmy(Date), Time = hms(Time) )
 
-png("plot1.png")
+png("plot1.png", width=480, height=480)
 
 hist(datas$Global_active_power, xlab="Global Active Power (kilowatts)", 
      main="Global Active Power", col="red")
